cae437463cc10713f6313c5a8533b25826012e06
alihf9674 <87331595+alihf9674@users.noreply.github.com>
2023-04-18 21:20:28 +0430

    Add function return type

--- system/traits/HasPostController.php
+++ system/traits/HasPostController.php
@@ -4,29 +4,29 @@ namespace System\Traits;
 
 trait HasPostController
 {
-      protected function imageTypeFilter($image)
-      {
-            $imagetypesRule = array('jpg', 'png', 'jpeg' , 'webp', 'gif');
-            if (isset($image)) {
-                  $imageType = explode('/', $image['type'])[1];
-                  in_array($imageType, $imagetypesRule);
-                  return true;
-            }
-            return false;
-      }
+    protected function imageTypeFilter($image): bool
+    {
+        $imagetypesRule = array('jpg', 'png', 'jpeg', 'webp', 'gif');
+        if (isset($image)) {
+            $imageType = explode('/', $image['type'])[1];
+            if (in_array($imageType, $imagetypesRule))
+                return true;
+        }
+        return false;
+    }
 
-      protected function timeCheck($timeValue)
-      {
-            $timeValue = $this->timeFilter($timeValue);
-            $presentTime = strtotime('now') - (60*60*24);            
-            if ($presentTime <= $timeValue) {
-                  return true;
-            }
-            return false;
-      }
+    protected function timeCheck($timeValue): bool
+    {
+        $timeValue = $this->timeFilter($timeValue);
+        $presentTime = strtotime('now') - (60 * 60 * 24);
+        if ($presentTime <= $timeValue) {
+            return true;
+        }
+        return false;
+    }
 
-      protected function timeFilter($time)
-      {
-            return substr($time, 0, 10);
-      }
+    protected function timeFilter($time): string
+    {
+        return substr($time, 0, 10);
+    }
 }
