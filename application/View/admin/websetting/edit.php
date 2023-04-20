874741639495f9ed8ef337ee1bd8d9f366d1d3c4
alihf9674 <87331595+alihf9674@users.noreply.github.com>
2023-04-20 22:27:53 +0430

    Show setting info

--- application/View/admin/websetting/edit.php
+++ application/View/admin/websetting/edit.php
@@ -16,27 +16,27 @@ $this->include('admin.layouts.sidebar');
         <form method="post" action="<?= $this->url('admin/setting/update') ?>" enctype="multipart/form-data">
             <div class="form-group">
                 <label for="title">Title</label>
-                <input type="text" class="form-control" id="title" name="title" placeholder="Enter title ..." value=""
-                       autofocus>
+                <input type="text" class="form-control" id="title" name="title" placeholder="Enter title ..."
+                       value="<?= $setting['title'] ?>" autofocus>
             </div>
             <div class="form-group">
                 <label for="description">Description</label>
                 <input type="text" class="form-control" id="description" name="description"
-                       placeholder="Enter title ..." value="" autofocus>
+                       placeholder="Enter title ..." value="<?= $setting['description'] ?>" autofocus>
             </div>
             <div class="form-group">
                 <label for="keywords">Keywords</label>
                 <input type="text" class="form-control" id="keywords" name="keywords" placeholder="Enter title ..."
-                       value="" autofocus>
+                       value="<?= $setting['keywords'] ?>" autofocus>
             </div>
             <div class="form-group">
-                <img style="width: 100px;" src="" alt="">
+                <img style="width: 100px;" src="<?= $this->asset($setting['logo']) ?>" alt="">
                 <hr/>
                 <label for="logo">Logo</label>
                 <input type="file" id="logo" name="logo" class="form-control-file" autofocus>
             </div>
             <div class="form-group">
-                <img style="width: 100px;" src="" alt="">
+                <img style="width: 100px;" src="<?= $this->asset($setting['icon']) ?>" alt="">
                 <hr/>
                 <label for="icon">Icon</label>
                 <input type="file" id="icon" name="icon" class="form-control-file" autofocus>
