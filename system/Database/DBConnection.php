5dc230f3aa23cf8de9164437ae41185126a3f096
alihf9674 <87331595+alihf9674@users.noreply.github.com>
2023-05-04 21:59:37 +0430

    Refactor PDOException

--- system/Database/DBConnection.php
+++ system/Database/DBConnection.php
@@ -4,6 +4,7 @@ namespace System\Database;
 
 use PDO;
 use PDOException;
+use Application\Exceptions\PDOConnectionException;
 
 class DBConnection
 {
@@ -13,14 +14,12 @@ class DBConnection
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
     );
-
     public function __construct()
     {
         try {
             $this->connection = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";", DB_USERNAME, DB_PASSWORD, $this->options);
-
         } catch (PDOException $e) {
-            echo "Error: Your connection has same error " . $e->getMessage();
+            throw new PDOConnectionException("Failed to connect to database : " . $e->getMessage());
         }
     }
 
