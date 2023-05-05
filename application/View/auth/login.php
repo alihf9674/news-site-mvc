96cd6413bef55634cb847a21ebb96ac5a154ad26
alihf9674 <87331595+alihf9674@users.noreply.github.com>
2023-05-05 23:50:49 +0430

    Add authenticate template

---
+++ application/View/auth/login.php
@@ -0,0 +1,62 @@
+<?php
+$this->include('auth.layouts.head-tag')
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Login</title>
+</head>
+<body>
+<div class="limiter">
+    <div class="container-login100">
+        <div class="wrap-login100">
+            <div class="login100-pic js-tilt" data-tilt>
+                <img src="" alt="IMG">
+            </div>
+            <form method="post" action="" class="login100-form validate-form">
+                    <span class="login100-form-title">
+                        Member Login
+                    </span>
+                <div class="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
+                    <input class="input100" type="text" name="email" placeholder="Email">
+                    <span class="focus-input100"></span>
+                    <span class="symbol-input100">
+                            <i class="fa fa-envelope" aria-hidden="true"></i>
+                        </span>
+                </div>
+                <div class="wrap-input100 validate-input" data-validate="Password is required">
+                    <input class="input100" type="password" name="password" placeholder="Password">
+                    <span class="focus-input100"></span>
+                    <span class="symbol-input100">
+                            <i class="fa fa-lock" aria-hidden="true"></i>
+                        </span>
+                </div>
+
+                <div class="container-login100-form-btn">
+                    <button type="submit" class="login100-form-btn">
+                        Login
+                    </button>
+                </div>
+                <div class="text-center p-t-12">
+                        <span class="txt1">
+                            Forgot
+                        </span>
+                    <a class="txt2" href="#">
+                        Username / Password?
+                    </a>
+                </div>
+                <div class="text-center p-t-136">
+                    <a class="txt2" href="<?= url('register') ?>">
+                        Create your Account
+                        <i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
+                    </a>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<?php
+$this->include('auth.layouts.scripts')
+?>
+</body>
+</html>
