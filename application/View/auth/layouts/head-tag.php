5a07c7466126d3611f92c78747eb1ae55e50b80a
alihf9674 <87331595+alihf9674@users.noreply.github.com>
2023-05-06 22:46:35 +0430

    Fix icon link

--- application/View/auth/layouts/head-tag.php
+++ application/View/auth/layouts/head-tag.php
@@ -1,7 +1,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->
-<link rel="icon" type="image/png" href="assets/images/icons/favicon.ico">
+<link rel="icon" type="image/png" href="<?= $this->asset('public/auth/assets/images/icons/favicon.ico')?>">
 <!--===============================================================================================-->
 <link rel="stylesheet" type="text/css" href="<?= $this->asset('public/auth/assets/vendor/bootstrap/css/bootstrap.min.css') ?>">
 <!--===============================================================================================-->
