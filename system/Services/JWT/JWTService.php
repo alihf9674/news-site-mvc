b304c7b6adc90749d924bb8e6376d805999f967b
alihf9674 <87331595+alihf9674@users.noreply.github.com>
2023-05-09 00:03:55 +0430

    Add jwt service

---
+++ system/Services/JWT/JWTService.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace System\Services\JWT;
+
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+class JWTService extends JWT
+{
+    private static $key = 'news_siteTokenSecretFromAdmin@thisSite';
+
+    const HS256 = 'HS256';
+    const HS384 = 'HS384';
+    const HS512 = 'HS512';
+    const RS256 = 'RS256';
+    const RS384 = 'RS384';
+    const RS512 = 'RS512';
+
+    public static function JwtEncode($data): string
+    {
+        return parent::encode($data, self::$key, self::HS256);
+    }
+
+    public static function JwtDecode($jwt): \stdClass
+    {
+        return parent::decode($jwt, new Key(self::$key, self::HS256));
+    }
+}
