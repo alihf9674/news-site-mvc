18997a9a58fe82c3ee4a1fef34d28d7f1656b45a
alihf9674 <87331595+alihf9674@users.noreply.github.com>
2023-05-09 00:15:29 +0430

    Initial register controller commit

--- application/Controllers/Auth/Register.php
+++ application/Controllers/Auth/Register.php
@@ -2,7 +2,35 @@
 
 namespace Application\Controllers\Auth;
 
-class Register extends \Application\Controllers\Controller
-{
+use Application\Controllers\Controller;
+use Application\Model\User as UserModel;
+use System\Services\JWT\JWTService;
 
+class Register extends Controller
+{
+    private array $formInput = ['username', 'email', 'password', 'confirm_password'];
+
+    public function registerView()
+    {
+        return $this->view('auth/register');
+    }
+
+    public function register($request)
+    {
+        if (empty($request))
+            $this->setWarningFlashMessage('فیلد ها باید پر شوند.');
+        if (!isValidInput($request, $this->formInput))
+            $this->setWarningFlashMessage('لطفا فیلد ها را به صورت صحیح وارد کنید.');
+        if (strlen($request['password']) < 6)
+            $this->setWarningFlashMessage('رمز عبور باید بیشتر از 6 کاراکتر باشد.');
+        if ($request['password'] !== $request['confirm_password'])
+            $this->setWarningFlashMessage('تکرار رمز با رمز باید یکسان باشد.');
+        if (!filter_var($request['email'], FILTER_VALIDATE_EMAIL))
+            $this->setWarningFlashMessage('لطفا فرمت ایمیل را به طور صحیح وارد کنید.');
+
+        $user = UserModel::getUserByEmail($request['email']);
+        if (is_null($user))
+            $this->setWarningFlashMessage('کاربر با این ایمیل در سیستم ثبت شده است لطفا به صفحه ورود بروید.');
+
+    }
 }
