14e39f2fce671635f6baec2d50c32446a2f5a770
alihf9674 <87331595+alihf9674@users.noreply.github.com>
2023-05-10 20:58:45 +0430

    Add success flash

--- system/traits/HasSetFlashMessage.php
+++ system/traits/HasSetFlashMessage.php
@@ -4,10 +4,25 @@ namespace System\Traits;
 
 trait HasSetFlashMessage
 {
-    protected function setWarningFlashMessage(string $message)
+    protected function setWarningFlashMessage(string $message, string $redirect_route = null)
     {
         flash('error', $message);
-        $this->back();
+        if (is_null($redirect_route)) {
+            $this->back();
+            die;
+        }
+        $this->redirect($redirect_route);
+        die;
+    }
+
+    protected function setSuccessFlashMessage(string $message, string $redirect_route = null)
+    {
+        flash('success', $message);
+        if (is_null($redirect_route)) {
+            $this->back();
+            die;
+        }
+        $this->redirect($redirect_route);
         die;
     }
 }
