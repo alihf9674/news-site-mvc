8ad56c8331544ee471f1891958d1707140c82766
alihf9674 <87331595+alihf9674@users.noreply.github.com>
2023-05-22 23:22:36 +0330

    Add session check

--- application/Controllers/Admin/Admin.php
+++ application/Controllers/Admin/Admin.php
@@ -1,13 +1,18 @@
 <?php
 
-namespace Application\Controllers\admin;
+namespace Application\Controllers\Admin;
 
 use Application\Controllers\Controller;
+use System\Auth\Auth;
 
 class Admin extends Controller
 {
     public function __construct()
     {
-
+        Auth::check();
+        if(Auth::user()->user_type != 'admin') {
+            $this->redirect('login');
+            exit;
+        }
     }
 }
