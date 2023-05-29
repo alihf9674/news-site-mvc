cd2bbfc701c300eb33fba940dc467d5e5c3838e9
alihf9674 <87331595+alihf9674@users.noreply.github.com>
2023-05-29 23:49:03 +0330

    Add app view template

---
+++ application/View/app/layouts/sidebar.php
@@ -0,0 +1,51 @@
+<div class="col-lg-4">
+    <div class="sidebars-area">
+        <div class="single-sidebar-widget editors-pick-widget">
+            <h6 class="title">انتخاب سردبیر</h6>
+                <div class="editors-pick-post">
+                    <div class="feature-img-wrap relative">
+                        <div class="feature-img relative">
+                            <div class="overlay overlay-bg"></div>
+                            <img class="img-fluid" src="" alt="">
+                        </div>
+                        <ul class="tags">
+                            <li><a href="#"></a></li>
+                        </ul>
+                    </div>
+                    <div class="details">
+                        <a href="">
+                            <h4 class="mt-20"></h4>
+                        </a>
+                        <ul class="meta">
+                            <li><a href="#"><span class="lnr lnr-user"></span>ادمین</a></li>
+                            <li><a href="#"><span class="lnr lnr-calendar-full"></span></a></li>
+                            <li><a href="#"><span class="lnr lnr-bubble"></span></a></li>
+                        </ul>
+                    </div>
+                </div>
+        </div>
+            <div class="single-sidebar-widget ads-widget">
+                <a href="">
+                    <img class="img-fluid" src="" alt="">
+                </a>
+            </div>
+        <div class="single-sidebar-widget most-popular-widget">
+            <h6 class="title">پر بحث ترین ها</h6>
+                <div class="single-list flex-row d-flex">
+                    <div class="thumb">
+                        <img src="" alt="">
+                    </div>
+                    <div class="details">
+                        <a href="">
+                            <h6></h6>
+                        </a>
+                        <ul class="meta">
+                            <li><a href="#"><span class="lnr lnr-calendar-full"></span></a></li>
+                            <li><a href="#"><span class="lnr lnr-bubble"></span></a></li>
+                        </ul>
+                    </div>
+                </div>
+
+        </div>
+    </div>
+</div>
